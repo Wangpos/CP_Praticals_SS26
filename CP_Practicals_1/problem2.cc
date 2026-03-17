cb881b00a88e56f294644d0b226b9ff92e295bc1
twangpodorji <02230311.cst@rub.edu.bt>
2026-03-17 16:22:18 +0600

    Refactor code structure for improved readability and maintainability

--- CP_Practicals_1/problem2.cc
+++ CP_Practicals_1/problem2.cc
@@ -9,22 +9,20 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
 int main() {
-     int N;
-     cin >> N;
+    int N;
+    cin >> N;
 
-     vector<int> arr(N);
-     for (int i = 0; i < N; i++) {
-          cin >> arr[i];
-     }
+    vector<int> arr(N);
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
 
-     for (int i = N - 1; i >= 0; i--) {
-          cout << arr[i];
-          if (i != 0) {
-               cout << " ";
-          }
-     }
-     cout << '\n';
+    for (int i = N - 1; i >= 0; i--) {
+        cout << arr[i];
+        if (i > 0) cout << " ";
+    }
 
-     return 0;
+    return 0;
 }
